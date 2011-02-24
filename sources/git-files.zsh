a2d1172bd3dc9ab659d987ff1bcd1c18ff452131
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2011-02-24 18:18:47 +0900

    add new source, git-files

---
+++ sources/git-files.zsh
@@ -0,0 +1,10 @@
+function zaw-src-git-files() {
+    git rev-parse --git-dir >/dev/null 2>&1
+    if [[ $? == 0 ]]; then
+        candidates=("${(ps:\0:)$(git ls-files -z)[1,-2]}")
+    fi
+    actions=("zaw-callback-edit-file" "zaw-callback-append-to-buffer")
+    act_descriptions=("edit file" "append to edit buffer")
+}
+
+zaw-register-src -n git-files zaw-src-git-files
