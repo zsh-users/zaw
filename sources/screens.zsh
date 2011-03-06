da8bb6b9b4d9bf255fc8240afb7d047cb1aff31f
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2011-03-07 01:24:29 +0900

    new source, screens

---
+++ sources/screens.zsh
@@ -0,0 +1,24 @@
+#
+# zaw-src-screens
+#
+# select screen session and attache it
+#
+
+function zaw-src-screens() {
+    local session state
+
+    screen -ls | awk '/^There (is a|are) screens? on:/ { next } /^[0-9]+ Sockets? in/ { exit } 1' | \
+        while read session state; do
+            candidates+=("${session}")
+            cand_descriptions+=("${(r:30:::::)session} ${state}")
+        done
+        actions=('zaw-callback-screens-attach')
+        act_descriptions=('attach session')
+}
+
+zaw-register-src -n screens zaw-src-screens
+
+function zaw-callback-screens-attach() {
+    BUFFER="screen -rx ${(q)1}"
+    zle accept-line
+}
