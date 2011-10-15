2069e4566c26173eae01343bc2e73731c397da43
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2011-10-15 21:37:23 +0900

    use "A" flag if is-at-least 4.3.10

--- zaw.zsh
+++ zaw.zsh
@@ -13,9 +13,16 @@
 function() {
 
 zmodload zsh/parameter
+autoload -U is-at-least
 
 local this_file="${funcsourcetrace[1]%:*}"
-local cur_dir="${this_file:A:h}"
+if is-at-least 4.3.10; then
+    # "A" flag (turn a file name into an absolute path with symlink
+    # resolution) is only available on 4.3.10 and latter
+    local cur_dir="${this_file:A:h}"
+else
+    local cur_dir="${this_file:h}"
+fi
 fpath+=("${cur_dir}/functions")
 
 autoload -U filter-select
