798e3d2ba8d6f14635784496392dfea544bfbfda
IWASAKI Yudai <yudai@arielworks.com>
2013-03-02 00:39:59 +0900

    Add append-to-to-edit-buffer to the actions

--- sources/git-branches.zsh
+++ sources/git-branches.zsh
@@ -7,8 +7,8 @@ function zaw-src-git-branches() {
         : ${(A)candidates::=${${(f)${branches_list}}#refs/}}
         : ${(A)cand_descriptions::=${${(f)${branches_list}}#refs/(remotes|heads|tags)/}}
     fi
-    actions=(zaw-src-git-branches-checkout zaw-src-git-branches-merge zaw-src-git-branches-merge-no-ff zaw-src-git-branches-merge-to zaw-src-git-branches-reset zaw-src-git-branches-create zaw-src-git-branches-delete)
-    act_descriptions=("check out" "merge" "merge no ff" "merge to" "reset" "create new branch from..." "delete")
+    actions=(zaw-src-git-branches-checkout zaw-callback-append-to-buffer zaw-src-git-branches-merge zaw-src-git-branches-merge-no-ff zaw-src-git-branches-merge-to zaw-src-git-branches-reset zaw-src-git-branches-create zaw-src-git-branches-delete)
+    act_descriptions=("check out" "append to edit buffer" "merge" "merge no ff" "merge to" "reset" "create new branch from..." "delete")
     options=()
 }
 
