14b5aa009ecae05e8dcdfa5a831914ac29ce67d8
muratayusuke <info@muratayusuke.com>
2013-06-26 15:42:33 +0900

    introduce git-recent-branches and git-recent-all-branches

---
+++ sources/git-recent-branches.zsh
@@ -0,0 +1,45 @@
+# zaw source for git branches sorted by commit date
+
+function zaw-src-git-recent-branches () {
+    git rev-parse --git-dir >/dev/null 2>&1
+    if [[ $? == 0 ]]; then
+        local branches="$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads)"
+        : ${(A)candidates::=${${(f)${branches}}#refs/}}
+        : ${(A)cand_descriptions::=${${(f)${branches}}#refs/(remotes|heads|tags)/}}
+    fi
+
+    actions=(zaw-src-git-branches-checkout zaw-src-git-branches-merge zaw-src-git-branches-merge-no-ff zaw-src-git-branches-merge-to zaw-src-git-branches-reset zaw-src-git-branches-create zaw-src-git-branches-delete)
+    act_descriptions=("check out" "merge" "merge no ff" "merge to" "reset" "create new branch from..." "delete")
+    options=()
+}
+
+function zaw-src-git-recent-all-branches () {
+    git rev-parse --git-dir >/dev/null 2>&1
+    if [[ $? == 0 ]]; then
+        local branches="$(git for-each-ref --format='%(refname)' --sort=-committerdate refs/heads refs/remotes)"
+        : ${(A)candidates::=${${(f)${branches}}#refs/}}
+        : ${(A)cand_descriptions::=${${(f)${branches}}#refs/(remotes|heads|tags)/}}
+    fi
+
+    actions=(zaw-src-git-branches-checkout zaw-src-git-branches-merge zaw-src-git-branches-merge-no-ff zaw-src-git-branches-merge-to zaw-src-git-branches-reset zaw-src-git-branches-create zaw-src-git-branches-delete)
+    act_descriptions=("check out" "merge" "merge no ff" "merge to" "reset" "create new branch from..." "delete")
+    options=()
+}
+
+function zaw-src-git-recent-branches-checkout () {
+    local b_type=${1%%/*}
+    local b_name=${1#(heads|remotes|tags)/}
+    case "$b_type" in
+        "heads"|"tags")
+            BUFFER="git checkout $b_name"
+            zle accept-line
+            ;;
+        "remotes")
+            BUFFER="git checkout -t $b_name"
+            zle accept-line
+            ;;
+    esac
+}
+
+zaw-register-src -n git-recent-branches zaw-src-git-recent-branches
+zaw-register-src -n git-recent-all-branches zaw-src-git-recent-all-branches
