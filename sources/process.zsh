4209ca3945f732b5b72d10ac26e1611bd8389016
Toshiki Teramura <toshiki.teramura@gmail.com>
2013-07-01 19:15:29 +0900

    change to use ps -ux instead of ps ux

--- sources/process.zsh
+++ sources/process.zsh
@@ -1,14 +1,16 @@
 # zaw source for processes
 
 function zaw-src-process () {
-    local process_list="$(ps -U $USER ux | sed -e '1d' -e '/ps -U/d')"
-    local process_desc="$(ps -U $USER ux | head -1)"
-    local pid_list="$(echo $process_list | awk '{print $2}')"
+    local ps_list title ps pid_list
+    ps_list="$(ps -ux --sort args)"
+    title="${${(f)ps_list}[1]}"
+    ps="$(echo $ps_list | sed '1d')"
+    pid_list="$(echo $ps | awk '{print $2}')"
     : ${(A)candidates::=${(f)pid_list}}
-    : ${(A)cand_descriptions::=${(f)process_list}}
+    : ${(A)cand_descriptions::=${(f)ps}}
     actions=(zaw-callback-append-to-buffer zaw-src-process-kill)
     act_descriptions=("insert" "kill")
-    options=(-t "$process_desc")
+    options=(-t "$title")
 }
 
 function zaw-src-process-kill () {
