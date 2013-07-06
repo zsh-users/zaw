99145dd8f52ee89f03f0c2d85c8cff6d260c7e66
muratayusuke <info@muratayusuke.com>
2013-07-06 20:53:04 +0900

    support added and modified

--- sources/git-status.zsh
+++ sources/git-status.zsh
@@ -4,11 +4,12 @@ function zaw-src-git-status() {
       local file_list="$(git status --porcelain)"
       : ${(A)candidates::=${(f)${file_list}}}
 
-      : ${(A)cand_descriptions::=${${(f)${file_list}}/ M /[modified]    }}
-      : ${(A)cand_descriptions::=${${(M)cand_descriptions}/M  /[staged]      }}
-      : ${(A)cand_descriptions::=${${(M)cand_descriptions}/A  /[staged(add)] }}
-      : ${(A)cand_descriptions::=${${(M)cand_descriptions}/UU /[conflict] }}
-      : ${(A)cand_descriptions::=${${(M)cand_descriptions}/\?\? /[untracked]   }}
+      : ${(A)cand_descriptions::=${${(f)${file_list}}/ M /[modified]     }}
+      : ${(A)cand_descriptions::=${${(M)cand_descriptions}/AM /[add|modified] }}
+      : ${(A)cand_descriptions::=${${(M)cand_descriptions}/M  /[staged]       }}
+      : ${(A)cand_descriptions::=${${(M)cand_descriptions}/A  /[staged(add)]  }}
+      : ${(A)cand_descriptions::=${${(M)cand_descriptions}/UU /[conflict]     }}
+      : ${(A)cand_descriptions::=${${(M)cand_descriptions}/\?\? /[untracked]    }}
 
     fi
 
@@ -18,28 +19,28 @@ function zaw-src-git-status() {
 }
 
 function zaw-src-git-status-add() {
-  local f_path=${1#(\?\? | M |M  |A  |UU )}
+  local f_path=${1#(\?\? | M |AM |M  |A  |UU )}
   local git_base="$(git rev-parse --show-cdup)"
   BUFFER="git add $git_base$f_path"
   zle accept-line
 }
 
 function zaw-src-git-status-add-p() {
-  local f_path=${1#(\?\? | M |M  |A  |UU )}
+  local f_path=${1#(\?\? | M |AM |M  |A  |UU )}
   local git_base="$(git rev-parse --show-cdup)"
   BUFFER="git add -p $git_base$f_path"
   zle accept-line
 }
 
 function zaw-src-git-status-reset() {
-  local f_path=${1#(\?\? | M |M  |A  |UU )}
+  local f_path=${1#(\?\? | M |AM |M  |A  |UU )}
   local git_base="$(git rev-parse --show-cdup)"
   BUFFER="git reset $git_base$f_path"
   zle accept-line
 }
 
 function zaw-src-git-status-checkout() {
-  local f_path=${1#(\?\? | M |M  |A  |UU )}
+  local f_path=${1#(\?\? | M |AM |M  |A  |UU )}
   local git_base="$(git rev-parse --show-cdup)"
   BUFFER="git checkout $git_base$f_path"
   zle accept-line
