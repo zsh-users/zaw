bf304524ae2cfc1d32bf73ae1e84def020b95e7c
Iwasaki Yudai <yudai@arielworks.com>
2015-06-26 16:23:02 -0700

    Quote file paths not to parse shell meta characters

--- sources/git-status.zsh
+++ sources/git-status.zsh
@@ -19,28 +19,28 @@ function zaw-src-git-status() {
 function zaw-src-git-status-add() {
   local f_path=${1#(\?\? | M |M  |A  )}
   local git_base="$(git rev-parse --show-cdup)"
-  BUFFER="git add $git_base$f_path"
+  BUFFER="git add '$git_base$f_path'"
   zle accept-line
 }
 
 function zaw-src-git-status-add-p() {
   local f_path=${1#(\?\? | M |M  |A  )}
   local git_base="$(git rev-parse --show-cdup)"
-  BUFFER="git add -p $git_base$f_path"
+  BUFFER="git add -p '$git_base$f_path'"
   zle accept-line
 }
 
 function zaw-src-git-status-reset() {
   local f_path=${1#(\?\? | M |M  |A  )}
   local git_base="$(git rev-parse --show-cdup)"
-  BUFFER="git reset $git_base$f_path"
+  BUFFER="git reset '$git_base$f_path'"
   zle accept-line
 }
 
 function zaw-src-git-status-checkout() {
   local f_path=${1#(\?\? | M |M  |A  )}
   local git_base="$(git rev-parse --show-cdup)"
-  BUFFER="git checkout $git_base$f_path"
+  BUFFER="git checkout '$git_base$f_path'"
   zle accept-line
 }
 
