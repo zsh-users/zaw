5e466526d8abc1865691d238b660b2a8591b61fc
William G Hatch <willghatch@gmail.com>
2015-09-20 17:50:15 -0600

    respect hist_find_no_dups in history source
    Closes #51 and #54.

--- sources/history.zsh
+++ sources/history.zsh
@@ -1,7 +1,11 @@
 zmodload zsh/parameter
 
 function zaw-src-history() {
-    cands_assoc=("${(@kv)history}")
+    if [[ -o hist_find_no_dups ]]; then
+        candidates=(${(@vu)history})
+    else
+        cands_assoc=("${(@kv)history}")
+    fi
     actions=("zaw-callback-execute" "zaw-callback-replace-buffer" "zaw-callback-append-to-buffer")
     act_descriptions=("execute" "replace edit buffer" "append to edit buffer")
     options=("-r" "-m" "-s" "${BUFFER}")
